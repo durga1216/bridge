95940b8fdd61de01d941618810372c4370f87ceb
susee <suseemukil@gmail.com>
2014-09-30 18:49:12 +0530

    change

---
+++ src/main/java/com/bridge/Index.java
@@ -0,0 +1,64 @@
+package com.bridge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/**
+ * Servlet implementation class Index
+ */
+@WebServlet("/Index")
+public class Index extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Index() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=response.getWriter();
+		try{
+			  HttpClient cli = new DefaultHttpClient();
+			  HttpGet get = new HttpGet("https://www.eventbrite.com/xml/event_search?app_key=3NC7TK5MON5ABMCYD4&city=chennai");
+		  HttpResponse res=cli.execute(get);
+		  BufferedReader in = new BufferedReader(
+                new InputStreamReader(res.getEntity().getContent()));
+			String line="";String str="";
+                while((line=in.readLine())!=null){
+                	str+=line;
+                }
+                out.println(str);
+		}
+		catch(Exception e){
+			out.println(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
