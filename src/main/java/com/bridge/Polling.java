83c6f8c8eb2213613f794aac6c82a40d82cc0d5b
rkurum <revathi@minddotss.com>
2014-12-18 08:18:40 +0530

    jb

--- src/main/java/com/bridge/Polling.java
+++ src/main/java/com/bridge/Polling.java
@@ -92,10 +92,7 @@ import com.jayway.jsonpath.JsonPath;
 public class Polling extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Polling() {
+        public Polling() {
         super();
     }
 
