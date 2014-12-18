4c66b14362d7f0d4088be346b41aa01fb18a9fcf
rkurum <revathi@minddotss.com>
2014-12-18 09:18:26 +0530

    kjhj

--- src/main/java/com/bridge/Polling.java
+++ src/main/java/com/bridge/Polling.java
@@ -95,7 +95,7 @@ public class Polling extends HttpServlet {
         public Polling() {
         super();
     }
- //polling test151
+ //polling test151677
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		final PrintWriter out=response.getWriter();
