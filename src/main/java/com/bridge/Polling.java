12f48a7e1de56b27f2add7ccf85e7a1e87f947e8
susee1592 <susee1592@gmail.com>
2014-12-18 14:54:56 +0530

    commit

--- src/main/java/com/bridge/Polling.java
+++ src/main/java/com/bridge/Polling.java
@@ -825,12 +825,12 @@ public class Polling extends HttpServlet {
 								x5=rs1.getString("xx5");x6=rs1.getString("xx6");
 								x7=rs1.getString("xx7");x8=rs1.getString("xx8");
 								x9=rs1.getString("xx9");x10=rs1.getString("xx10");
-								/*x11=rs1.getString("xx11");x12=rs1.getString("xx12"); 
+								x11=rs1.getString("xx11");x12=rs1.getString("xx12"); 
 								x13=rs1.getString("xx13");x14=rs1.getString("xx14");
 								x15=rs1.getString("xx15");x16=rs1.getString("xx16");
 								x17=rs1.getString("xx17");x18=rs1.getString("xx18");
 								x19=rs1.getString("xx19");
-								x20=rs1.getString("xx20");*/ptag=rs1.getString("ptag");
+								x20=rs1.getString("xx20");ptag=rs1.getString("ptag");
 								exres=rs1.getString("exres");shname=rs1.getString("shname");
 								parpol=rs1.getString("parpol");unipol=rs1.getString("unipol");
 							} 
@@ -843,11 +843,11 @@ public class Polling extends HttpServlet {
 									ArrayList arx5=new ArrayList();ArrayList arx6=new ArrayList();
 									ArrayList arx7=new ArrayList();ArrayList arx8=new ArrayList();
 									ArrayList arx9=new ArrayList();ArrayList arx10=new ArrayList();
-									/*ArrayList arx11=new ArrayList();ArrayList arx12=new ArrayList();
+									ArrayList arx11=new ArrayList();ArrayList arx12=new ArrayList();
 									ArrayList arx13=new ArrayList();ArrayList arx14=new ArrayList();
 									ArrayList arx15=new ArrayList();ArrayList arx16=new ArrayList();
 									ArrayList arx17=new ArrayList();ArrayList arx18=new ArrayList();
-									ArrayList arx19=new ArrayList();ArrayList arx20=new ArrayList();*/
+									ArrayList arx19=new ArrayList();ArrayList arx20=new ArrayList();
 									
 									arr=JsonPath.read(str, unipol);
 									
@@ -881,7 +881,7 @@ public class Polling extends HttpServlet {
 								    if(!x10.equals("null")){
 								    	arx10=JsonPath.read(str,x10);
 								    }
-								 /*   if(!x11.equals("null")){
+								   if(!x11.equals("null")){
 								    	arx11=JsonPath.read(str,x11);
 								    }
 								    if(!x12.equals("null")){
@@ -910,7 +910,7 @@ public class Polling extends HttpServlet {
 								    }
 								    if(!x20.equals("null")){
 								    	arx20=JsonPath.read(str,x20);
-								    }*/
+								    }
 									//check the initial array size
 									if(arr1.size()==0){
 										arr1=arr;
@@ -936,7 +936,7 @@ public class Polling extends HttpServlet {
 											xo9=arx9.get(m);xx9=xo9.toString();}
 											if(!x10.equals("null")){
 											xo10=arx10.get(m);xx10=xo10.toString();}	
-											/*if(!x11.equals("null")){
+											if(!x11.equals("null")){
 											xo11=arx11.get(m);xx11=xo11.toString();}
 											if(!x12.equals("null")){
 											xo12=arx12.get(m);xx12=xo12.toString();}
@@ -955,13 +955,13 @@ public class Polling extends HttpServlet {
 											if(!x19.equals("null")){
 											xo19=arx19.get(m);xx19=xo19.toString();}
 											if(!x20.equals("null")){
-											xo20=arx20.get(m);xx20=xo20.toString();}*/
+											xo20=arx20.get(m);xx20=xo20.toString();}
 											
 											//insert every records in array for json or xml Rpc's
 											xx[1]=xx1;xx[2]=xx2;xx[3]=xx3;xx[4]=xx4;xx[5]=xx5;
 											xx[6]=xx6;xx[7]=xx7;xx[8]=xx8;xx[9]=xx9;xx[10]=xx10;
-										//	xx[11]=xx11;xx[12]=xx12;xx[13]=xx13;xx[14]=xx14;xx[15]=xx15;
-										//	xx[16]=xx16;xx[17]=xx17;xx[18]=xx18;xx[19]=xx19;xx[20]=xx20;
+											xx[11]=xx11;xx[12]=xx12;xx[13]=xx13;xx[14]=xx14;xx[15]=xx15;
+											xx[16]=xx16;xx[17]=xx17;xx[18]=xx18;xx[19]=xx19;xx[20]=xx20;
 											
 											//Join the parsed elements into valid json or xml
 											String[] slt=exres.split("@@");
@@ -1009,7 +1009,7 @@ public class Polling extends HttpServlet {
 												xo9=arx9.get(m);xx9=xo9.toString();}
 												if(!x10.equals("null")){
 												xo10=arx10.get(m);xx10=xo10.toString();}
-												/*if(!x11.equals("null")){
+												if(!x11.equals("null")){
 												xo11=arx11.get(m);xx11=xo11.toString();}
 												if(!x12.equals("null")){
 												xo12=arx12.get(m);xx12=xo12.toString();}
@@ -1028,7 +1028,7 @@ public class Polling extends HttpServlet {
 												if(!x19.equals("null")){
 												xo19=arx19.get(m);xx19=xo19.toString();}
 												if(!x20.equals("null")){
-												xo20=arx20.get(m);xx20=xo20.toString();}*/
+												xo20=arx20.get(m);xx20=xo20.toString();}
 										
 												xx[1]=xx1;xx[2]=xx2;xx[3]=xx3;xx[4]=xx4;xx[5]=xx5;
 												xx[6]=xx6;xx[7]=xx7;xx[8]=xx8;xx[9]=xx9;xx[10]=xx10;
