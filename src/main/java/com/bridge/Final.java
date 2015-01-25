c2448c0e3fc1b1ad0c83a19b924ca18827d35457
susee1592 <susee1592@gmail.com>
2015-01-25 08:17:05 +0530

    commi

--- src/main/java/com/bridge/Final.java
+++ src/main/java/com/bridge/Final.java
@@ -971,24 +971,17 @@ public class Final extends HttpServlet {
  			   	}		 	 			   	
 /**   Parsing and mapping ends  ------ Action block starts from here	  **/
  			   	
- 			   	if("null".equals(c1)){
- 			   		ActionClass act=new ActionClass(da,xx1,xx2,xx3,xx4,xx5,xx6,xx7,xx8,xx9,xx10,xx11,xx12,xx13,xx14,xx15,xx16,xx17,xx18,xx19,xx20,orurl,shname);
- 			   		String str1=act.start();
-		   		}
- 			   	else{  
- 			   		if((JsonPath.read(str,c1)).equals(cv1) && (JsonPath.read(str,c2)).equals(cv2) || (JsonPath.read(str,c3)).equals(cv3) ){
- 			   			ActionClass act=new ActionClass(da,xx1,xx2,xx3,xx4,xx5,xx6,xx7,xx8,xx9,xx10,xx11,xx12,xx13,xx14,xx15,xx16,xx17,xx18,xx19,xx20,orurl,shname);
- 			   			String str1=act.start();
- 			   		}
- 			   	}
+ 			   	ActionClass act=new ActionClass(da,xx1,xx2,xx3,xx4,xx5,xx6,xx7,xx8,xx9,xx10,xx11,xx12,xx13,xx14,xx15,xx16,xx17,xx18,xx19,xx20,orurl,shname);
+ 			   	String str1=act.start();
+ 			   		
  				String outpt=da+str+"\n\n"+xx1+xx2+xx3+xx4+xx5+"\n\n"+orurl+"\n\n"+check;
  				session.setAttribute("samp", outpt);
-// 				try {
-// 					PreparedStatement ps1=con.prepareStatement("insert into testing (data) values('"+check+"')");
-// 					ps1.executeUpdate();
-// 				} catch (SQLException e1) {
-// 					e1.printStackTrace();
-// 				}		
+ 				try {
+ 					PreparedStatement ps1=con.prepareStatement("insert into testing (data) values('"+check+"')");
+ 					ps1.executeUpdate();
+ 				} catch (SQLException e1) {
+ 					e1.printStackTrace();
+ 				}		
 			} catch (Exception e1) {
 				out.println(e1);
 			}
