98f3ad1f15c32461ea701a511e4715a17099cb21
susee1592 <susee1592@gmail.com>
2015-03-17 12:46:10 +0530

    comi

--- src/main/java/com/thread/thread/Sqltest.java
+++ src/main/java/com/thread/thread/Sqltest.java
@@ -3,6 +3,8 @@ package com.thread.thread;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import com.bridge.Util;
 
@@ -12,10 +14,15 @@ public class Sqltest {
 		// TODO Auto-generated method stub
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-		    Connection con=DriverManager.getConnection("jdbc:mysql://106.51.133.146:3306/mynddotz_susee","mynddotz_susi","MindPulpy12");
-		    PreparedStatement st=con.prepareStatement("insert into xerodata (name,desc) values ('susee','success')");
-		    st.executeUpdate();
-            
+		    Connection con=DriverManager.getConnection("jdbc:mysql://127.12.212.2:3306/bridge", "admin7R9w6e8", "5n4gq2Pz4q_b");
+		    PreparedStatement paa=con.prepareStatement("insert into testpol (error,str,tempid) values('sfsasfas','remote','login')");
+			paa.executeUpdate();
+//			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+//			Date d1 = format.parse("2015-03-01");
+//			Date d2 = format.parse("2015-03-02");
+//			long diff = d2.getTime() - d1.getTime();
+//			long diffDays = diff / (24 * 60 * 60 * 1000);
+//            System.out.println(diffDays);
 		} catch (Exception e) {
 			System.out.println(e);
 		}
